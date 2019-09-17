610788e4c2d8015dbc7f72ae38331fa225563da9
douglas-hobatista <53202820+douglas-hobatista@users.noreply.github.com>
2019-09-17 09:31:01 -0300

    Add files via upload

---
+++ Priceproducts/Showprice/Controller/Index/index.php
@@ -0,0 +1,16 @@
+<?php  
+
+namespace Priceproducts\Showprice\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+/**
+ * Catalog index page controller.
+ */
+class Index extends Action 
+{
+    public function execute()
+    {
+        echo "Módulo Carregado";
+    }
+}
---
+++ Priceproducts/Showprice/registration.php
@@ -0,0 +1,6 @@
+<?php
+
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Priceproducts_Showprice', __DIR__);
